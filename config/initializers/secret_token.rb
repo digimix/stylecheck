228176769e40c0e32b07615734d138ee30c944af
Michael Salafia <michael@digimix.us>
2014-03-18 18:33:23 -0400

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stylecheck::Application.config.secret_key_base = '7f5aa0a8f11fc5981c7e7ec2fc24423485c3913f6da035604f14a78fa596eecc7164ed572b9fc4c306355d4c810a64302fc30940cca7f29515839da0614eb0bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stylecheck::Application.config.session_store :cookie_store, key: '_stylecheck_session'
